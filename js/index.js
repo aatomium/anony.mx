fc2b228e7ee54f41e8c3378114bf9272a10532c7
harrypotter360 <orian.latroupe@gmail.com>
2023-07-10 22:22:12 +0200

    well so i updated cause it was realy ugly

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ send.addEventListener("click",send_message);
 
 function send_message(){
     console.log("ezeff");
-    socket.emit("message",document.getElementById("message_to_send").value);
+    socket.emit("message",document.getElementById("pseudo").value,document.getElementById("message_to_send").value);
 }
 import ("../socket/socket.io.js").then(({f1}) => {
     socket = io("http://192.168.1.127:3000", {
@@ -15,6 +15,29 @@ import ("../socket/socket.io.js").then(({f1}) => {
     socket.on('connect', function (socket) {
         console.log('Connected!');
     });
+    socket.on('send_messages', (data) => {
+        data.forEach((item, index) => {
+            final_str = item.pseudo + ' - ' + item.message;
+            var tag = document.createElement("p");
+            var text = document.createTextNode(final_str);
+            tag.appendChild(text);
+            var element = document.getElementById("messages_container");
+            element.appendChild(tag);
+        })
+        // Traiter le message et envoyer une réponse au client si nécessaire
+    });
+    socket.on("new_message",(pseudo,data) => {
+        console.log("test");
+        final_str = pseudo + ' - ' + data;
+        var tag = document.createElement("p");
+        var text = document.createTextNode(final_str);
+        tag.appendChild(text);
+        var element = document.getElementById("messages_container");
+        element.appendChild(tag);
+    })
 
 
 })
+var form = document.getElementById("form_send_message");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
